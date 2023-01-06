d99c578a6f3c9dd87cda011548d72ab4a53f12ab
David Garcia Hernandez <david.garciahz@gmail.com>
2023-01-06 11:56:17 +0100

    Update HelloWorld.java

--- src/main/java/test/HelloWorld.java
+++ src/main/java/test/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello "+args[0]+". Your second name is:"+args[1]);
+		System.out.println("Hello "+args[0]+". Your second ugly name is:"+args[1]);
 	}
 
 	public String getName(String in) {
